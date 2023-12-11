214cc57f4119f29f06d59edf193e0ced2c180b0f
Ahmed Elhadidy <ElhadiAA@mopm.gov.sa>
2023-12-11 14:26:04 +0300

    first

---
+++ lib/expenses.dart
@@ -0,0 +1,41 @@
+import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _reqisteredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('your List of expenses'),
+          Expanded(
+            child: ExpensesList(expenses: _reqisteredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
